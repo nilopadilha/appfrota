0add9f81cad4fdaf870536253fa1b6f80ecd1643
nilopadilha <nilopadilha@gmail.com>
2020-10-02 11:45:24 -0300

    commit inicial projeto appfrota

---
+++ src/main/java/br/com/dinamo/appfrota/AppfrotaApplication.java
@@ -0,0 +1,13 @@
+package br.com.dinamo.appfrota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppfrotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppfrotaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dinamo/appfrota/AppfrotaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dinamo.appfrota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppfrotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
